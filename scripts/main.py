1edbbe1c2a80a809d2e619b699f1d3c0ad831802
baizhif <135608974+baizhif@users.noreply.github.com>
2023-07-07 08:21:48 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ import gradio as gr
 import os
 import subprocess
 from zipfile import ZipFile
+from fastapi import FastAPI
 
 from modules import script_callbacks
 
@@ -66,5 +67,43 @@ def on_ui_tabs():
         uploader.change(fn=uploadeFile, inputs=[uploader,text], outputs=[label_output])
         cmd_text.submit(fn=runCmd,inputs=[cmd_text],outputs=label_output)
         return [(ui_component, "uploader", "extension_uploader_tab")]
+    
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+def on_app_started(_: gr.Blocks, app: FastAPI) -> None:
+    @app.websocket("/ws")
+    async def websocket_endpoint(websocket: WebSocket):
+        await manager.connect(websocket)
+        print("新客户")
+        try:
+            while True:
+                data = await websocket.receive_text()
+                await manager.send_personal_message(f"You wrote: {data}", websocket)
+                await manager.broadcast(f"Client # says: {data}")
+        except WebSocketDisconnect:
+            manager.disconnect(websocket)
+            await manager.broadcast(f"Client # left the chat")
+
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
+script_callbacks.on_app_started(on_app_started)
