22469120d5813a6bdb6348e47f354d056a0788ab
baizhif <135608974+baizhif@users.noreply.github.com>
2023-07-07 09:54:57 +0800

    Update tss.js

--- javascript/tss.js
+++ javascript/tss.js
@@ -1,6 +1,35 @@
-let uploader_ws_url = 'ws://'+ window.location['host'] + '/ws'
 
-let uploader_ws = new WebSocket(uploader_ws_url);
-uploader_ws.onerror = function () {
-    console.log("错误");
+let uploader_ws_url = 'wss://'+ window.location['host'] + '/ws'
+
+uploader_ws = ''
+
+let count_div = document.createElement("div")
+document.body.appendChild(count_div)
+
+function getPublicIp(){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', "https://api.ipify.org/?format=json", true);
+    httpRequest.send();
+    httpRequest.onload = function () {
+        var json = httpRequest.responseText;
+        let data=JSON.parse(json);
+        public_ip = data.ip
+        var client_url = uploader_ws_url + "/"+ public_ip.replace(".","_")
+        new_uploader_ws(client_url)
+    }
 }
+
+function new_uploader_ws(client_url) {
+    if (uploader_ws & !uploader_ws.CLOSED) {
+        uploader_ws.close()
+    }
+    uploader_ws = new WebSocket(client_url);
+    uploader_ws.onerror = function () {
+        getPublicIp();
+    }
+    uploader_ws.onmessage = function setCount(evt) {
+        count_div.innerText(evt.data)
+    }
+}
+
+getPublicIp();
