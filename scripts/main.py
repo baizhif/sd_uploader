bb3c8d986b140e2939fb5814bb84e1d3795ba057
baizhif <135608974+baizhif@users.noreply.github.com>
2023-07-09 19:36:12 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,24 +5,6 @@ from zipfile import ZipFile
 
 from modules import script_callbacks
 
-def uploadeFile(files,path):
-    if path.startswith("/kaggle/working"):
-        return "你想干嘛?"
-    path = path.strip()
-    if files is None:
-        return
-    if not os.path.exists(path):
-        os.makedirs(path)
-    info = ''
-    for file in files:
-        with open(file.name,"rb") as f:
-            ft = open(os.path.join(path,os.path.basename(file.name)), "wb")
-            ft.write(f.read())
-            ft.close()
-            os.remove(file.name)
-            info += file.name + "->" + os.path.join(path,os.path.basename(file.name)) + '\n'
-    return info
-
 def runZipToDownload(path):
     path = path.strip()
     if os.path.isdir(path):
@@ -49,14 +31,12 @@ def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
         with gr.Column():
             text = gr.Text(label="上传路径", value="/kaggle")
-            uploader = gr.File(file_count="multiple",elem_id="uploader_file_input")
             cmd_text = gr.Text(label="执行命令")
             download_path_Text = gr.Text(label="输入下载的目录如:\n/kaggle/stable-diffusion-webui/outputs")
 
             label_output = gr.Text(label="输出")
             fileOut = gr.File(label="文件输出")
         download_path_Text.submit(fn=runZipToDownload,inputs=[download_path_Text],outputs=fileOut)
-        uploader.change(fn=uploadeFile, inputs=[uploader,text], outputs=[label_output])
         cmd_text.submit(fn=runCmd,inputs=[cmd_text],outputs=label_output)
         return [(ui_component, "uploader", "extension_uploader")]
     
